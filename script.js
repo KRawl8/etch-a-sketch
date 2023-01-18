c4cde282bc3b85fd677d379033e4f8f3f5d6ce3c
Kieran <49917350+KRawl8@users.noreply.github.com>
2023-01-18 18:42:37 +0000

    Add option to select a random color
    There is now a button that selects a random color to draw with

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let gridDimension = 16;
 let newGridDimensions;
-
+let paintColor = "black";
 
 const grid = document.querySelector('#canvas');
 
 
 const makeGrid = () => {
 
-    while (grid.firstChild) {
+    while (grid.firstChild) { // Removes all the boxes so new ones can be made
         grid.removeChild(grid.firstChild);
     }
 
@@ -22,18 +22,33 @@ const makeGrid = () => {
             const heightAndWidth = (640 / gridDimension) + "px";
             gridDiv.setAttribute('style', `height: ${heightAndWidth}; width: ${heightAndWidth};`);
             gridDiv.classList.add('gridDiv');
-            gridDiv.addEventListener('mouseenter', changeColor);
+            gridDiv.addEventListener('mouseenter', paint);
             grid.appendChild(gridDiv);
         }
     }
 };
 
-const changeColor = (e) => {
-    e.currentTarget.style.backgroundColor = "black";
-    e.currentTarget.style.borderColor = "black";
+const buttonColor = document.querySelectorAll('.colorButton');
+buttonColor.forEach((button) => {
+    button.addEventListener('click', () => {
+        switch (button.id) {
+            case "black":
+                paintColor = "blue";
+                break;
+            case "random":
+                paintColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+                break;
+        }
+    })
+}
+);
+
+const paint = (e) => {
+    e.currentTarget.style.backgroundColor = paintColor;
+    e.currentTarget.style.borderColor = paintColor;
 };
 
-const gridButton = document.querySelector('#gridDimensions'); //When user clicks the button they choose the new value for gridDimension
+const gridButton = document.querySelector('#gridDimensions'); // When user clicks the button they choose the new value for gridDimension
 gridButton.addEventListener('click', () => {
     possibleGridDimensions = prompt("What dimension would you like the grid to have?");
     if (possibleGridDimensions >= 1 && possibleGridDimensions <= 100) {
